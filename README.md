886b0a98039b962cc6ece033c17596a7658ad6b2
Thiago Matos Santos <tms.42@hotmail.com>
2023-10-16 09:07:30 -0300

    commit with update

---
+++ README.md
@@ -0,0 +1,33 @@
+# Extensão para adicionar notas no moodle
+
+Essa é uma extensão para o **Chrome** com a finalidade adicionar notas na plataforma moodle, sem a necessidade de enviar as notas via CSV pois tem consumo alto de memória e tempo o que gera uma sobrecarga no sistema e em alguns casos uma queda do mesmo. Siga as instruções para a instalação e bom proveito. 
+
+***Obs: em breve estará disponível nas extensões do Chrome.***
+
+## Instalação
+
+***Passo 1***
+
+Baixar em formato zip no GIT.                                                   
+
+![enter image description here](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDFybDJpNnA0b3Zhcm54eW4zdzM1d2hkcTY3MjV3dXRkbWJ1cXd3eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/pFTTHolgLDtOBKJl10/giphy.gif)
+
+***Passo 2***
+
+Extrair o arquivo baixado. Note que deve ter esses arquivos(index, script, README e manifest e style) após a extração.
+
+![enter image description here](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExOXFjdXJ0azBkaW5uZnB0OXRyOGZhZTUxa2Q1MmU3YXpzaW9tcm16eCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/5PWX2OJvB62Ei6Cl70/giphy.gif)
+
+***Passo 3***
+Carregar a extensão baixada através do gerenciador de extensões do Chrome.
+
+***Obs: é necessário habilitar o modo desenvolvedor do Chrome.***
+
+![enter image description here](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExcTk3bmxvZWlzeTR3cmE2em8xMnVnZXBnMWQ5N3dldHRmcDN3MW9hbCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/MxESJLzseUcBVNrDaB/giphy.gif)
+
+![enter image description here](https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExb3htOGxzcG5kdDc0cWw2dHA1YjBmcngzanhzMDd3YzRuYjdvaXBzYiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9cw/ZgYssKUaDT4dTWyjTt/giphy.gif)
+
+***Passo 4***
+Selecionar e copiar a coluna nome e nota do excel e colar na extensão. Após isso é só clicar em enviar, dessa forma será substituído a nota dos alunos via navegador.
+
+![enter image description here](https://media.giphy.com/media/G1wxnHh2a9IKFspskQ/giphy.gif)
---
+++ script.js
@@ -0,0 +1,101 @@
+//https://code.tutsplus.com/pt/tutorials/developing-google-chrome-extensions--net-33076
+//https://acervolima.com/extensao-do-chrome-marcador-do-youtube/
+
+
+const form = document.querySelector('form');
+const bt_reset = document.querySelector('.bt_reset')
+const bt_update = document.querySelector('.bt_update')
+
+let dados = []
+const addNotas = (dados)=>{    
+    // document.body.style.background = 'red';
+    const [nome_, nota_] = Object.keys(dados[0])
+    console.log(nome_)
+    let users = document.querySelectorAll(".user.cell.c1")
+          
+    users.forEach((user)=>{   
+        const aluno = dados.find(el=>String(el[nome_]).toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+        ==user.children[1].innerText.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "")) 
+        
+        if(aluno){
+            user.nextElementSibling.nextElementSibling.children[1].value = aluno[nota_]==""?0:aluno[nota_]
+        }            
+    })    
+}
+
+const resetNotas = ()=>{           
+    let users = document.querySelectorAll(".user.cell.c1")
+     
+    users.forEach((user)=>{   
+        user.nextElementSibling.nextElementSibling.children[1].value = ""           
+    })    
+}
+
+const updateNotas = (dados)=>{    
+    // document.body.style.background = 'red';
+    const [nome_, nota_] = Object.keys(dados[0])
+    console.log(nome_)
+    let users = document.querySelectorAll(".user.cell.c1")
+          
+    users.forEach((user)=>{   
+        const aluno = dados.find(el=>String(el[nome_]).toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "")
+        ==user.children[1].innerText.toLowerCase().trim().normalize('NFD').replace(/[\u0300-\u036f]/g, "")) 
+        
+        if(aluno){
+            if(user.nextElementSibling.nextElementSibling.children[1].value != aluno[nota_]){
+                user.nextElementSibling.nextElementSibling.children[1].value = aluno[nota_]==""?0:aluno[nota_]
+            }
+        }            
+    })     
+}
+
+const convert_text_in_json =(notas_)=>{
+    let notas = notas_.split('\n')      
+    notas.shift()
+
+    const dados =[]
+    notas.forEach(el=>{
+        const dados_obj = {}
+        let [nome, nota] = (el.split('\t'))
+        Object.assign(dados_obj, {'nome':nome, 'nota':nota})
+        console.log(dados_obj)
+        dados.push(dados_obj)    
+    })    
+    return dados
+}
+
+form.addEventListener('submit', async (event)=>{
+    dados =(convert_text_in_json(document.querySelector('.input').value))
+    event.preventDefault();
+
+    // alert('está funcionando')    
+    const [tab] = await chrome.tabs.query({active:true, currentWindow:true});
+    chrome.scripting.executeScript({
+        target: {tabId:tab.id},
+        function: addNotas,
+        args:[dados]
+    });
+
+})
+
+bt_update.addEventListener('click', async (event)=>{
+    alert("updateNotas")
+    const [tab] = await chrome.tabs.query({active:true, currentWindow:true});
+    //console.log(para a execução pelo chrome)
+    chrome.scripting.executeScript({
+        target: {tabId:tab.id},
+        function: updateNotas,
+        args:[dados]
+    });
+})
+
+bt_reset.addEventListener('click', async (event)=>{
+    alert("resetNotas")
+    const [tab] = await chrome.tabs.query({active:true, currentWindow:true});
+    //console.log(para a execução pelo chrome)
+    chrome.scripting.executeScript({
+        target: {tabId:tab.id},
+        function: resetNotas
+    });
+})
+
